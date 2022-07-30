23e0f99aed37b176a4e9d575784494344e9d6528
Fardeen khan <104645858+fardeen2811khan@users.noreply.github.com>
2022-07-30 23:27:52 +0530

    Add files via upload

---
+++ build/precache-manifest.10f6271d0264734e8898b1e61d7181c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9e4d8a75f32667b8738d5ab3d4db45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa54d99c6bde5ddd753",
+    "url": "/static/js/2.97b3ad86.chunk.js"
+  },
+  {
+    "revision": "a0c70bdb8ade7f674ab8",
+    "url": "/static/js/main.87e38335.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
